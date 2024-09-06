8e66f7c9c348440dd4d11586349ec4ba5bb64eca
MayurSankhla <msankhla.covetus@gmail.com>
2024-09-06 14:41:42 +0530

    Firebase Configuration
    set-upped firebase

--- android/app/src/main/kotlin/com/example/cold_storage_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/cold_storage_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cold_storage_flutter
+package com.appcoldstorage.coldstorage
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
